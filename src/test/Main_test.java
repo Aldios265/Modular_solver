c4e34459f1f0ead9f59a76f2f957c5cdce612d0c
aldios <kynick@mail.ru>
2023-03-15 10:11:43 +0300

    Рабочий Calculator_test2. Solver-ы возвращают десятки.

--- src/test/Main_test.java
+++ src/test/Main_test.java
@@ -1,19 +1,9 @@
 package test;
 
-import java.util.Arrays;
-
 public class Main_test {
     public static void main(String[] args) {
         Calculator_test2.solverMain();
 
-
-//        Object[][] array = {{1d,2d,"-"}, {1d,"-","-"}, {"-","-","-"}};
-//        int[] equationCodeArray = Calculator_test2.arrayEquationCodeDetermine(array);
-//        for (int i :equationCodeArray) {
-//            System.out.println(i);
-//        }
-
-//        Calculator_test2.solverMain();
         }
     }
 
