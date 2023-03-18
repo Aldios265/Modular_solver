d07f559453a3e9965edd1e196bf6d3f3ea7c3042
aldios <kynick@mail.ru>
2023-03-18 15:19:49 +0300

    убраны поля treeName и tree

--- src/test/MenuMain.java
+++ src/test/MenuMain.java
@@ -3,6 +3,7 @@ package test;
 public class MenuMain {
     public static void main(String[] args) {
         test.menu.MainMenu mainMenu = new test.menu.MainMenu();
+        test.menu.Settings settings = new test.menu.Settings();
         MenuExecutor.mainMenu(mainMenu);
     }
 }
--- src/test/menu/Settings.java
+++ src/test/menu/Settings.java
@@ -1,4 +1,25 @@
 package test.menu;
 
-public class Settings {
+public class Settings implements Menu{
+    String treeName = "Settings";
+    String[] tree = {"SelectLanguage", "Select"};
+
+    String name = "Настройки";
+    String[] possibleOptions = {"Выбрать язык", "Выбрать что-то еще"};
+
+    public String getTreeName() {
+        return treeName;
+    }
+
+    public String[] getTree() {
+        return tree;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String[] getOptions() {
+        return possibleOptions;
+    }
 }
